45c41d18acb628d887163f0ac0d0d301e6ae086e
Adventureandre <adventureandre@hotmail.com>
2023-10-28 00:25:54 -0300

    OrderStatus enum

--- src/main/java/com/expertcustom/course/entities/Order.java
+++ src/main/java/com/expertcustom/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.expertcustom.course.entities;
 
+import com.expertcustom.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -17,6 +18,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -24,10 +28,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -40,6 +45,15 @@ public class Order implements Serializable {
 
     public Instant getMoment() {
         return moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+        this.orderStatus = orderStatus.getCode();}
     }
 
     public void setMoment(Instant moment) {
